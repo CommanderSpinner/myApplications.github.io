3857d7be86d5072c861a9d29aa8942cbccd1c864
CommanderSpinner <klaus.kutulu@gmail.com>
2025-01-08 11:28:49 +0100

    finnished darkmode

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // switch to ligth/dark mode
+
+let darkmode = false;
+function changeTheme(){
+  console.log("theme changed");
+  if (!darkmode) {
+    darkmode = true;
+    document.documentElement.style.setProperty("--background_color", "rgb(48, 48, 48)");
+    document.documentElement.style.setProperty("--text_color", "rgb(206, 206, 206)");
+    document.documentElement.style.setProperty("--list_background_color", "rgb(48, 48, 48)");
+  } else if (darkmode) {
+    darkmode = false;
+    document.documentElement.style.setProperty("--background_color", "#eaeaea");
+    document.documentElement.style.setProperty("--text_color", "rgb(48, 48, 48)");
+    document.documentElement.style.setProperty("--list_background_color", "white");
+  }
+}
