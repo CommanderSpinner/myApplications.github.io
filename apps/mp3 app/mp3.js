7a85de7b6e42350d784628c04c4973cbfd08e0ec
CommanderSpinner <klaus.kutulu@gmail.com>
2025-01-13 11:45:38 +0100

    made own folder for mp3 web app

---
+++ apps/mp3 app/mp3.js
@@ -0,0 +1,25 @@
+// mp3.js
+
+console.log("mp3 player script is laoded");
+
+// Select the file input element
+const fileInput = document.getElementById('fileInput');
+
+// Add an event listener to handle the files when the user clicks the submit button
+document.getElementById('submitButton').addEventListener('click', () => {
+    console.log("submit");
+    // Get the selected files from the input
+    const files = fileInput.files;
+
+    if (files.length > 0) {
+        console.log(`You have uploaded ${files.length} files.`);
+        
+        // Loop through the FileList and process each file
+        Array.from(files).forEach((file, index) => {
+            console.log(`File ${index + 1}:`, file);
+
+        });
+    } else {
+        console.log('No files selected.');
+    }
+});
